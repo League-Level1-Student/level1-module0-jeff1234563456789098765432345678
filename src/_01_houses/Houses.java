d887e58496bfc0d24e2ba8455b19d8326577b589
League Student <league@iMac-38.attlocal.net>
2020-02-20 18:28:01 -0800

    Houses :Smile:

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,34 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
+	Robot shiv = new Robot();
+
 	public void run() {
 		
+		shiv.moveTo(100, 500);
+		shiv.setSpeed(30);
+			for (int i = 0; i < 10; i++) {
+				drawHouse("small");
+				drawHouse("medium");
+				drawHouse("large");
+			}
 	}
-}
+
+	void drawHouse(String height) {
+		shiv.penDown();
+		shiv.move(100);
+		shiv.turn(90);
+		shiv.move(20);
+		shiv.turn(90);
+		shiv.move(100);
+		shiv.turn(270);
+		shiv.setPenColor(0, 255, 17);
+		shiv.move(20);
+		shiv.turn(270);
+		shiv.setPenColor(0,0,0);
+	}
+	
+	
+	}
