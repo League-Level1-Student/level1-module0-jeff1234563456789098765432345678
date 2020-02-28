6c9b6a5393ae38c1333f0cf4ae3aba3730d711b5
League Student <league@iMac-38.attlocal.net>
2020-02-27 18:29:31 -0800

    Houses :Smile:

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,34 +1,60 @@
 package _01_houses;
 
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
 	Robot shiv = new Robot();
 
 	public void run() {
-		
 		shiv.moveTo(100, 500);
 		shiv.setSpeed(30);
-			for (int i = 0; i < 10; i++) {
-				drawHouse("small");
-				drawHouse("medium");
-				drawHouse("large");
+		//drawFlatRoof();
+			for (int i = 0; i < 3; i++) {
+				drawHouse("small", Color.BLUE);
+				drawHouse("medium", Color.DARK_GRAY);
+				drawHouse("large", Color.cyan);
 			}
+		
 	}
+	
 
-	void drawHouse(String height) {
+	void drawHouse(String height,Color buildingColor) {
+		int house;
+		if(height.equals("small")) {
+			house = 50;
+		
+		}else if (height.equals("medium")) {
+			 house = 100;
+			
+		}else {
+		 house = 150;	
+		}
+		shiv.setPenColor(buildingColor);
 		shiv.penDown();
-		shiv.move(100);
-		shiv.turn(90);
-		shiv.move(20);
-		shiv.turn(90);
-		shiv.move(100);
+		shiv.move(house);
+		drawFlatRoof();
+		shiv.move(house);
 		shiv.turn(270);
 		shiv.setPenColor(0, 255, 17);
 		shiv.move(20);
 		shiv.turn(270);
 		shiv.setPenColor(0,0,0);
 	}
-	
-	
+	void drawPointyRoof() {
+		//write code for pointy roof here
+		
 	}
+	void drawFlatRoof() {
+		shiv.turn(90);
+		shiv.move(20);
+		shiv.turn(90);
+	}
+		}
+	
+	
+	
+	
